94c22626ade0876f85888ef4a3a1e9ddc415458e
SorenBlattBendtsen <s164521@student.dtu.dk>
2024-01-03 14:08:04 +0100

    soren M4

---
+++ Shared_exercises/M4_soren/data.py
@@ -0,0 +1,28 @@
+import torch
+
+
+def mnist():
+    """Return train and test dataloaders for MNIST."""
+    # exchange with the corrupted mnist dataset
+    path = "/Users/sorenbendtsen/Documents/GitHub/dtu_mlops/data/corruptmnist/"
+    train_images, train_labels = [], []
+    for i in range(1, 6):
+        train_images.append(torch.load(path + "train_images_" + str(i) + ".pt"))
+        train_labels.append(torch.load(path + "train_target_" + str(i) + ".pt"))
+
+    test_images = torch.load(path + "test_images.pt")
+    test_labels = torch.load(path + "test_target.pt")
+    
+    # stack the tensors
+    train_images = torch.cat(train_images)
+    train_labels = torch.cat(train_labels)
+    
+    # convert to torch tensors
+    train = torch.utils.data.TensorDataset(train_images, train_labels)
+    test = torch.utils.data.TensorDataset(test_images, test_labels)
+
+    # create dataloaders
+    train = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True)
+    test = torch.utils.data.DataLoader(test, batch_size=32, shuffle=False)
+    
+    return train, test
---
+++ Shared_exercises/M4_soren/model.py
@@ -0,0 +1,20 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class MyAwesomeModel(nn.Module):
+    """My awesome model."""
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = F.log_softmax(self.fc3(x), dim=1)
+
+        return x
