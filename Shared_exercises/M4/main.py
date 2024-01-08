647e99d413139424b674090afef46ab5148533e3
jacdals97 <jacdals@hotmail.com>
2024-01-08 17:08:29 +0100

    try to commit M4

--- Shared_exercises/M4/main.py
+++ Shared_exercises/M4/main.py
@@ -4,7 +4,8 @@ from model import MyAwesomeModel
 import matplotlib.pyplot as plt
 from data import mnist
 
-path = './Shared_exercises/M4/'
+path = "./Shared_exercises/M4/"
+
 
 @click.group()
 def cli():
@@ -15,14 +16,14 @@ def cli():
 @click.command()
 @click.option("--lr", default=1e-3, help="learning rate to use for training")
 @click.option("--epochs", default=10, help="learning rate to use for training")
-def train(lr,epochs):
+def train(lr, epochs):
     """Train a model on MNIST."""
     print("Training day and night")
     print(lr)
 
     # TODO: Implement training loop here
     model = MyAwesomeModel()
-    train_set, _ = mnist(path=path+'data/')
+    train_set, _ = mnist(path=path + "data/")
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = torch.nn.NLLLoss()
     train_losses = []
@@ -31,20 +32,19 @@ def train(lr,epochs):
         for images, labels in train_set:
             images = images.view(images.shape[0], -1)
             optimizer.zero_grad()
-            
+
             log_ps = model(images)
             loss = criterion(log_ps, labels)
             loss.backward()
             optimizer.step()
             train_losses.append(loss.item())
-    
-    #save plot of training loss
-    plt.plot(train_losses)
-    plt.savefig(path + 'train_losses.png')
-    #save model
-    torch.save(model.state_dict(), path+'checkpoint.pth')
 
-            
+    # save plot of training loss
+    plt.plot(train_losses)
+    plt.savefig(path + "train_losses.png")
+    # save model
+    torch.save(model.state_dict(), path + "checkpoint.pth")
+
 
 @click.command()
 @click.argument("model_checkpoint")
@@ -68,7 +68,8 @@ def evaluate(model_checkpoint):
             equals = top_class == labels.view(*top_class.shape)
             accuracy = torch.mean(equals.type(torch.FloatTensor))
             accuracies.append(accuracy.item())
-    print(f'Accuracy: {sum(accuracies)/len(accuracies)}')
+    print(f"Accuracy: {sum(accuracies)/len(accuracies)}")
+
 
 cli.add_command(train)
 cli.add_command(evaluate)
